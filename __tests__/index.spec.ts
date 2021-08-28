1e9ed1f50f83fe4ddf73e5b81fb7408670467fa4
afeiship <1290657123@qq.com>
2021-08-28 22:52:46 +0800

    fix: update

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,7 +1,19 @@
+import weiboUpload from '@afeiship/weibo-picupload';
 import fn from '../src';
 
+jest.setTimeout(50 * 1000);
+
 describe('api.basic', () => {
-  test('normail single value case', () => {
-    expect(fn({ rel: true })).toBe(undefined);
+  test('normail single value case', (done) => {
+    fn({
+      src: 'https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg',
+      cover: './__tests__/beta_s.png',
+      gravity: 'northwest'
+    }).then((res) => {
+      weiboUpload([res]).then((res2) => {
+        console.log(res2);
+        done();
+      });
+    });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,29 @@
-export default (): void => {
-  console.log('hello');
+import sharp from 'sharp';
+import NxFsOpen from '@jswork/next-fs-open';
+
+interface Options {
+  /**
+   * 需要打水印的图片
+   */
+  src: string;
+  /**
+   * 水印图片
+   */
+  cover: string;
+  /**
+   * 生成后的图片路径
+   */
+  dst?: string;
+  /**
+   * 水印的位置
+   */
+  gravity?: sharp.Gravity;
+}
+
+export default async (inOptions: Options): Promise<Buffer> => {
+  const { src, cover, gravity } = inOptions;
+  const buf = await NxFsOpen.from(src);
+  return sharp(buf)
+    .composite([{ input: cover, gravity }])
+    .toBuffer();
 };
