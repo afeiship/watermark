8294a32f702cdae1925a06a624eb546fb818f626
afeiship <1290657123@qq.com>
2021-08-28 23:20:30 +0800

    docs: udpate usage

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ import watermark from '@jswork/watermark';
 
 watermark({
   src: 'https://pic.rmb.bdstatic.com/7f7a8d7b247d3aa430010f10a5765239.jpeg',
-  cover: './__tests__/beta_s.png',
+  cover: './__tests__/beta_s.png',  // local.png/remote.png/remote.svg
   gravity: 'northwest'
 }).then((res) => {
   // res: buffer
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import sharp from 'sharp';
+import sharp, { OverlayOptions } from 'sharp';
 import NxFsOpen from '@jswork/next-fs-open';
 
-interface Options {
+interface Options extends OverlayOptions {
   /**
    * 需要打水印的图片
    */
@@ -11,18 +11,16 @@ interface Options {
    */
   cover: string;
   /**
-   * 生成后的图片路径
-   */
-  dst?: string;
-  /**
    * 水印的位置
    */
   gravity?: sharp.Gravity;
 }
 
 export default async (inOptions: Options): Promise<Buffer> => {
-  const { src, cover, gravity } = inOptions;
+  const { src, cover, gravity, ...options } = inOptions;
   const buf = await NxFsOpen.from(src);
   const input = await NxFsOpen.from(cover);
-  return sharp(buf).composite([{ input, gravity }]).toBuffer();
+  return sharp(buf)
+    .composite([{ input, gravity, ...options }])
+    .toBuffer();
 };
